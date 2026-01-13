9bdd1b861443693a48cbe5329b9485fa539d7be0
nhusna01 <nrlhsn1310@gmail.com>
2026-01-13 19:22:14 +0800

    Delete preprocess.py

--- preprocess.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import streamlit as st
-from configuration import GOOGLE_SHEET_URL
-
-@st.cache_data(ttl=15)   # refresh every 15 seconds
-def load_data():
-    df = pd.read_csv(GOOGLE_SHEET_URL)
-    return df
-
